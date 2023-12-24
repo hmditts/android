edf44c8068f4c98f026fea63744bc8854e0f7abb
hmditts <138617615+hmditts@users.noreply.github.com>
2023-12-24 13:36:37 +0300

    Update main.java

--- main.java
+++ main.java
@@ -21,3 +21,17 @@ tv.setTypeface(null, Typeface.ITALIC);
 // AND
 tv.setPaintFlags(tv.getPaintFlags()|Paint.UNDERLINE_TEXT_FLAG);
 (ll, Typeface.ITALIC);
+
+
+TextView tv = (TextView)findViewById(R.id.textViewOne);
+tv.setTypeface(null, Typeface.BOLD_ITALIC);
+// OR
+tv.setTypeface(null, Typeface.BOLD|Typeface.ITALIC);
+// OR
+tv.setTypeface(null, Typeface.BOLD);
+// OR
+tv.setTypeface(null, Typeface.ITALIC);
+// AND
+tv.setPaintFlags(tv.getPaintFlags()|Paint.UNDERLINE_TEXT_FLAG);
+(ll, Typeface.BOLD);
+
